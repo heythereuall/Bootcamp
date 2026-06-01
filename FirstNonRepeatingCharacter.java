590d672277ce75475ca1d4fc0bc96c643d70758e
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-06-01 23:55:53 +0530

    practice question

---
+++ FirstNonRepeatingCharacter.java
@@ -0,0 +1,15 @@
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String s="aabbbbcddd";
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+    }
+}
