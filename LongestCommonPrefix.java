2ca8ca2704271802eef78fcfd328379d66796e17
Aditi Jaiswal <95347598+heythereuall@users.noreply.github.com>
2026-06-02 00:03:41 +0530

    practice question

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0, prefix.length()-1);
+            }
+            if(prefix.isEmpty()){
+                System.out.println("No match");
+            }
+        }
+        System.out.println(prefix);
+    }
+}
